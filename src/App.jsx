0b40d727385463033a12607c8bbf8020934f0710
Favour Okoh <okohfavour91@gmail.com>
2023-09-11 09:25:05 +0100

    Refactor: Added animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import logo from "./assets/zapay-logo.png"
+import logoWhite from "./assets/zapay-logo-white.png"
 import globeIcon from "./assets/globe-icon.svg"
 import smilingMan from "./assets/smiling-man-with-phone.png"
 import playIcon from "./assets/play-icon.svg"
@@ -13,12 +14,20 @@ import moneyImg from "./assets/money.png"
 import graphImg from "./assets/graph.png"
 import cartIcon from "./assets/cart.svg"
 import './App.css'
+import { useEffect } from "react"
 
 function App() {
+  useEffect(() => {
+  });
 
   return (
     <>
-      <header className="py-5 bg-bgWhite">
+      <div className="fixed top-0 left-0 w-full min-h-[100vh] z-[10000] flex justify-center items-center animate-[hide_1s_0.8s_ease-in-out_forwards]">
+        <div className="w-1/2 bg-zapayGreen h-[100vh] animate-[slideDown_1s_ease-in-out_forwards]"></div>
+        <div className="w-1/2 bg-zapayGreen  h-[100vh] animate-[slideUp_1s_ease-in-out_forwards]"></div>
+        <img className="absolute animate-[hide_1s_0.4s_ease-in-out_forwards]" src={logoWhite} alt="" />
+      </div>
+      <header className="py-5 bg-bgWhite translate-y-[-100%] animate-[slideDown_0.5s_1.1s_ease_forwards]" style={{animationTimingFunction: 'cubic-bezier(0.250, 0.460, 0.450, 0.940)'}}>
         <nav className="container mx-auto flex justify-between items-center px-3">
           <div className="flex items-center">
             <a href="#" className="mr-8">
@@ -50,45 +59,44 @@ function App() {
             <div className="flex flex-col gap-12 md:flex-row">
               <div className="flex flex-col justify-between items-center w-full md:w-[40%] pt-10 pb-5">
                 <div className="text-[#202626] mb-10 text-center md:text-left">
-                    <h1 className="font-semibold leading-[1] text-5xl md:text-6xl mb-4
-                    ">Better Money solution for your everyday needs</h1>
-                    <p className="text-lg mb-4">Seamless Life changing Financial solutions for traders and business owners across the middle-east</p>
+                    <h1 className="font-semibold leading-[1] text-5xl md:text-6xl mb-4 animate-fadeInLeft">Better Money solution for your everyday needs</h1>
+                    <p className="text-lg mb-4 animate-fadeInLeft">Seamless Life changing Financial solutions for traders and business owners across the middle-east</p>
                     <div className="flex md:justify-start justify-center items-center space-x-4">
-                      <a className="text-bgWhite font-medium bg-zapayGreen py-2 px-4 rounded-md cursor-pointer hover:shadow-md hover:scale-95">Get Started</a>
-                      <a className="flex items-center text-[#202626] font-medium bg-[#F9E6D2] py-2 px-4 rounded-md cursor-pointer hover:shadow-md"><img className="mr-1" src={playIcon} alt="" />How it works</a>
+                      <a className="text-bgWhite font-medium bg-zapayGreen py-2 px-4 rounded-md cursor-pointer hover:scale-95 hover:shadow-md">Get Started</a>
+                      <a className="flex items-center text-[#202626] font-medium bg-[#F9E6D2] py-2 px-4 rounded-md cursor-pointer hover:scale-95 hover:shadow-md"><img className="mr-1" src={playIcon} alt="" />How it works</a>
                     </div>
                 </div>
                 <div className="w-auto md:w-full">
                   <div className="flex items-center">
-                    <div className="w-12 h-12 flex overflow-hidden bg-[#804FBF] border-2 border-zapayGreen rounded-full -ml-2">
+                    <div className="w-12 h-12 flex overflow-hidden bg-[#804FBF] border-2 border-zapayGreen rounded-full -ml-2 animate-fadeInLeft">
                       <img src={manImg1} alt="" />
                     </div>
-                    <div className="w-12 h-12 flex overflow-hidden bg-[#EFC169] border-zapayGreen rounded-full -ml-2">
+                    <div className="w-12 h-12 flex overflow-hidden bg-[#EFC169] border-zapayGreen rounded-full -ml-2 animate-fadeInLeft" style={{animationDelay: '1.8s'}}>
                       <img src={girlImg1} alt="" />
                     </div>
-                    <div className="w-12 h-12 flex overflow-hidden bg-[#56BDDD] border-2 border-zapayGreen rounded-full -ml-2">
+                    <div className="w-12 h-12 flex overflow-hidden bg-[#56BDDD] border-2 border-zapayGreen rounded-full -ml-2 animate-fadeInLeft" style={{animationDelay: '2s'}}>
                       <img src={manImg2} alt="" />
                     </div>
-                    <div className="w-12 h-12 flex overflow-hidden bg-[#FFCAD4] border-2 border-zapayGreen rounded-full -ml-2">
+                    <div className="w-12 h-12 flex overflow-hidden bg-[#FFCAD4] border-2 border-zapayGreen rounded-full -ml-2 animate-fadeInLeft" style={{animationDelay: '2.2s'}}>
                       <img src={girlImg2} alt="" />
                     </div>
-                    <div className="w-12 h-12 flex justify-center items-center overflow-hidden bg-[#F9E5D2] border-2 border-zapayGreen rounded-full -ml-2">
+                    <div className="w-12 h-12 flex justify-center items-center overflow-hidden bg-[#F9E5D2] border-2 border-zapayGreen rounded-full -ml-2 animate-fadeInLeft" style={{animationDelay: '2.5s'}}>
                       <span className="text-zapayGreen text-lg font-semibold">+5M</span>
                     </div>
                   </div>
-                  <p className="text-[#C7C0BB]">registered merchants globally</p>
+                  <p className="text-[#C7C0BB] animate-fadeInUp">registered merchants globally</p>
                 </div>
               </div>
                 <div className="w-full md:w-[60%] hidden md:flex  items-center relative pl-[3.2rem] translate-x-[20%] bg-[#E9E6E0] rounded-l-[5rem] before:absolute before:content-[''] before:top-0 before:left-4 before:bg-[url('/src/assets/hero-right-pattern.png')] before:w-full before:h-full before:-z-1">
                   <img className="h-[400px] z-10 flex self-end" src={smilingMan} alt="Man using Zapay" />
-                  <div className="p-2 flex justify-center items-center absolute bottom-[22%] right-[52%] rounded-lg gap-2 z-20 backdrop-blur-sm" style={{background: 'linear-gradient(99deg, rgba(250, 253, 253, 0.45) 27.81%, rgba(250, 253, 253, 0.40) 88.68%)', boxShadow: '4px 4px 4px 0px rgba(0, 0, 0, 0.10)'}}>
+                  <div className="p-2 flex justify-center items-center absolute bottom-[22%] right-[52%] rounded-lg gap-2 z-20 backdrop-blur-sm animate-fadeInRight" style={{background: 'linear-gradient(99deg, rgba(250, 253, 253, 0.45) 27.81%, rgba(250, 253, 253, 0.40) 88.68%)', boxShadow: '4px 4px 4px 0px rgba(0, 0, 0, 0.10)'}}>
                     <img className="w-10 h-10" src={graphImg} alt="" />
                     <div className="">
                       <p className="text-md font-semibold">$17,000</p>
                       <p className="text-xs font-medium">Total Earnings</p>
                     </div>
                   </div>
-                  <div className="p-2 absolute bottom-[5%] left-[10%] rounded-lg gap-2 z-20 backdrop-blur-sm" style={{background: 'linear-gradient(99deg, rgba(250, 253, 253, 0.45) 27.81%, rgba(250, 253, 253, 0.40) 88.68%)', boxShadow: '4px 4px 4px 0px rgba(0, 0, 0, 0.10)'}}>
+                  <div className="p-2 absolute bottom-[5%] left-[10%] rounded-lg gap-2 z-20 backdrop-blur-sm animate-fadeInUp" style={{background: 'linear-gradient(99deg, rgba(250, 253, 253, 0.45) 27.81%, rgba(250, 253, 253, 0.40) 88.68%)', boxShadow: '4px 4px 4px 0px rgba(0, 0, 0, 0.10)'}}>
                       <div className="flex justify-center items-center gap-2">
                           <div className="p-1 bg-white rounded-md">
                             <img className="w-4 h-4" src={cartIcon} alt="" /> 
@@ -100,7 +108,7 @@ function App() {
                         <p className="text-gray-500 text-[10px]">+26.71% increase</p>
                       </div>
                   </div>
-                  <div className="p-3 flex justify-center items-center absolute bottom-[30%] left-[-80px] rounded-lg gap-2 z-20 backdrop-blur-sm bg-[rgba(250, 253, 253, 0.60)]" style={{boxShadow: '4px 4px 4px 0px rgba(0, 0, 0, 0.10)'}}>
+                  <div className="p-3 flex justify-center items-center absolute bottom-[30%] left-[-80px] rounded-lg gap-2 z-20 backdrop-blur-sm bg-[rgba(250, 253, 253, 0.60)] animate-fadeInLeft" style={{boxShadow: '4px 4px 4px 0px rgba(0, 0, 0, 0.10)'}}>
                     <img className="w-5 h-5" src={moneyImg} alt="" />
                       <p className="text-xs font-semibold">Receive Payments Globally</p>
                   </div>
@@ -110,7 +118,7 @@ function App() {
         </section>
         <div className="container mx-auto px-3 mt-5 md:mb-5 space-x-5">
           <div className="flex flex-col md:flex-row justify-around items-center p-7" style={{background: 'linear-gradient(90deg, rgba(232, 230, 224, 0.00) -3.95%, #E8E6E0 49.03%, rgba(232, 230, 224, 0.00) 99.95%)'}}>
-            <p className="font-medium max-w-[150px]">We are trusted by reliable partners</p>
+            <p className="font-medium max-w-[150px] animate-fadeInUp">We are trusted by reliable partners</p>
             <img src={applePayLogo} alt="" />
             <img src={paykasaLogo} alt="" />
             <img src={bankMusactLogo} alt="" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,45 @@ export default {
       colors:{
         zapayGreen: "#024E4C",
         bgWhite: "#FBF6F2"
+      },
+      animation: {
+        'fadeInLeft': 'fadeInLeft 0.6s 1.5s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+        'fadeInRight': 'fadeInRight 0.6s 1.5s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+        'fadeInUp': 'fadeInUp 0.6s 1.5s cubic-bezier(0.390, 0.575, 0.565, 1.000) both',
+        'bounceInTop': 'bounceInTop 1.1s 1.3s ease-in-out both',
+      },
+      keyframes: {
+        slideDown: {
+          '100%': { translate: '0 100%' },
+        },
+        slideUp: {
+          '100%': { translate: '0 -100%' },
+        },
+        hide: {
+          '100%': { display: 'none' },
+        },
+        fadeInLeft: {
+          '0%': { translate: '-100px', opacity: '0' },
+          '100%': { translate: '0', opacity: '1' },
+        },
+        fadeInRight: {
+          '0%': { translate: '100px', opacity: '0' },
+          '100%': { translate: '0', opacity: '1' },
+        },
+        fadeInUp: {
+          '0%': { translate: '0 100px', opacity: '0' },
+          '100%': { translate: '0 0', opacity: '1' },
+        },
+        bounceInTop: {
+          '0%': { translate: '0 -300px', opacity: '0' },
+          '38%': { translate: '0 0'},
+          '55%': { translate: '0 -45px'},
+          '72%': { translate: '0 0'},
+          '81%': { translate: '0 -15px'},
+          '90%': { translate: '0 0', opacity: '1'},
+          '95%': { translate: '0 -8px'},
+          '100%': { translate: '0 0', opacity: '1' },
+        }
       }
     },
   },
